a3c26becc9b39e79cabbfa5a305c37387844e014
Korvek <e.gattermann@li-ga.de>
2024-11-29 11:59:09 +0100

    Alien Skript update

--- Assets/Scripts/Alien.cs
+++ Assets/Scripts/Alien.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Alien : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = new Vector2(transform.position.x+0.01f,transform.position.y);
     }
 }
